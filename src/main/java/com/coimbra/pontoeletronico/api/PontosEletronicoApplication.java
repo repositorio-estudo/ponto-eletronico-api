0127ef09d91ca9e50c73aea155854bc034b2e963
repositorio-estudo <rodrigo.carrilho.soa@gmail.com>
2020-10-26 20:35:50 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/coimbra/pontoeletronico/api/PontosEletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.coimbra.pontoeletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontosEletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coimbra/pontoeletronico/api/PontosEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coimbra.pontoeletronico.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
